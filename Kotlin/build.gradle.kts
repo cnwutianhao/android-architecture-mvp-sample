bcc64814730b2692709fded4ac75f3cdd1c15db0
Tyhoo Wu <cnwutianhao@163.com>
2021-08-14 06:55:13 +0800

    Migrating build logic from Groovy to Kotlin.

--- Kotlin/build.gradle.kts
+++ Kotlin/build.gradle.kts
@@ -1,6 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext.kotlin_version = "1.5.21"
 
     repositories {
         google()
@@ -8,8 +7,8 @@ buildscript {
     }
 
     dependencies {
-        classpath "com.android.tools.build:gradle:4.2.2"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:7.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -23,6 +22,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class.java) {
+    delete(rootProject.buildDir)
 }
--- Kotlin/settings.gradle.kts
+++ Kotlin/settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "MVP_KOTLIN"
-include ':app'
+include(":app")
